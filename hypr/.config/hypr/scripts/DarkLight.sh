bc5b78e93c909e46f88213850f1d02258b9abda8
manokel01 <manokel@gmail.com>
2026-02-23 20:14:10 +0200

    Void: fixed broken terminal ui

--- hypr/.config/hypr/scripts/DarkLight.sh
+++ hypr/.config/hypr/scripts/DarkLight.sh
@@ -32,7 +32,7 @@ done
 
 
 # Initialize swww if needed
-swww query || swww-daemon --format xrgb
+# swww query || swww-daemon --format argb
 
 # Set swww options
 swww="swww img"
@@ -140,7 +140,7 @@ else
 fi
 
 # Update wallpaper using swww command
-$swww "${next_wallpaper}" $effect
+# $swww "${next_wallpaper}" $effect
 
 
 # Set Kvantum Manager theme & QT5/QT6 settings
@@ -219,7 +219,7 @@ set_custom_gtk_theme "$next_mode"
 update_theme_mode
 
 
-${SCRIPTSDIR}/WallustSwww.sh &&
+# ${SCRIPTSDIR}/WallustSwww.sh &&
 
 sleep 2
 # kill process
