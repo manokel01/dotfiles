b892f7fb6c0d2301a6f6b48ee906d55f644107f8
manokel01 <manokel@gmail.com>
2026-03-15 01:44:09 +0200

    implemented better night light automatic script and other waybar/hyprland configs

--- scripts/.local/bin/nl_toggle.sh
+++ scripts/.local/bin/nl_toggle.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-if pgrep -x gammastep > /dev/null; then
-    # If it's running, kill it
-    pkill -x gammastep
-else
-    # If it's not running, start it and completely detach it from Waybar
-    gammastep -O 3500 > /dev/null 2>&1 & disown
-fi
+killall -SIGUSR1 gammastep
